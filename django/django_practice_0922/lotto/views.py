a74c22b89d8fb65e0779a8dd156e3658a1c679f4
kimdakyeom <74820869+kimdakyeom@users.noreply.github.com>
2022-09-23 16:27:56 +0900

    django modify

--- django/django_practice_0922/lotto/views.py
+++ django/django_practice_0922/lotto/views.py
@@ -2,72 +2,37 @@ from django.shortcuts import render
 import random
 
 def lotto(request):
-  all = list(range(1, 45))
-  numbers1 = random.sample(all, 6)
-  numbers2 = random.sample(all, 6)
-  numbers3 = random.sample(all, 6)
-  numbers4 = random.sample(all, 6)
-  numbers5 = random.sample(all, 6)
-  lotto = [3, 11, 15, 29, 35, 44]
+  lotto_result_list = []
+  today = [3, 11, 15, 29, 35, 44]
   bonus = 10
-  cnt1 = 0
-  cnt2 = 0
-  cnt3 = 0
-  cnt4 = 0
-  cnt5 = 0
-  flag1 = False
-  flag2 = False
-  flag3 = False
-  flag4 = False
-  flag5 = False
 
-  for num in numbers1:
-    if num in lotto:
-      cnt1 += 1
-    if num == bonus:
-      flag1 = True
+  for _ in range(5):
+    lotto = random.sample(range(1, 46), 6)
 
-  for num in numbers2:
-    if num in lotto:
-      cnt2 += 1
-    if num == bonus:
-      flag2 = True
+    cnt = 0
+    flag = False
+    for num in lotto:
+      if num in today:
+        cnt += 1
+      if num == bonus:
+        flag = True
 
-  for num in numbers3:
-    if num in lotto:
-      cnt3 += 1
-    if num == bonus:
-      flag3 = True
-
-  for num in numbers4:
-    if num in lotto:
-      cnt4 += 1
-    if num == bonus:
-      flag4 = True
-
-  for num in numbers5:
-    if num in lotto:
-      cnt5 += 1
-    if num == bonus:
-      flag5 = True
+    if cnt == 6:
+      lotto_result_list.append({'lotto': lotto, 'result': '1등'})
+    elif cnt == 5 and flag == True:
+      lotto_result_list.append({'lotto': lotto, 'result': '2등'})
+    elif cnt == 5 and flag == False:
+      lotto_result_list.append({'lotto': lotto, 'result': '3등'})
+    elif cnt == 4:
+      lotto_result_list.append({'lotto': lotto, 'result': '4등'})
+    elif cnt == 3:
+      lotto_result_list.append({'lotto': lotto, 'result': '5등'})
+    else:
+      lotto_result_list.append({'lotto': lotto, 'result': '꽝'})
 
   context = {
-    'numbers1': numbers1,
-    'numbers2': numbers2,
-    'numbers3': numbers3,
-    'numbers4': numbers4,
-    'numbers5': numbers5,
-    'lotto': lotto,
+    'lotto_result_list' : lotto_result_list,
+    'today': today,
     'bonus': bonus,
-    'cnt1': cnt1,
-    'cnt2': cnt2,
-    'cnt3': cnt3,
-    'cnt4': cnt4,
-    'cnt5': cnt5,
-    'flag1': flag1,
-    'flag2': flag2,
-    'flag3': flag3,
-    'flag4': flag4,
-    'flag5': flag5,
   }
   return render(request, 'lotto.html', context)
